a32f949fdf5277fb4e2b2123b6c7eff0a6bfef15
AnaGeorgieska <68482597+AnaGeorgieska@users.noreply.github.com>
2021-12-12 20:13:44 +0100

    Add files via upload

---
+++ Домашна 3/Web Application/src/main/java/mk/ukim/finki/dians/model/Hospital.java
@@ -0,0 +1,109 @@
+package mk.ukim.finki.dians.model;
+
+import lombok.Data;
+
+@Data
+public class Hospital {
+    private Long id;
+    private Double lon;
+    private Double lat;
+    private String name;
+    private String webSite;
+    private String phone;
+    private String driveThrough;
+    private String openHours;
+    private String email;
+    private String building;
+    private String wheelchair;
+    public Hospital() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public void setWebSite(String webSite) {
+        this.webSite = webSite;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDriveThrough() {
+        return driveThrough;
+    }
+
+    public void setDriveThrough(String driveThrough) {
+        this.driveThrough = driveThrough;
+    }
+
+    public String getOpenHours() {
+        return openHours;
+    }
+
+    public void setOpenHours(String openHours) {
+        this.openHours = openHours;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getWheelchair() {
+        return wheelchair;
+    }
+
+    public void setWheelchair(String wheelchair) {
+        this.wheelchair = wheelchair;
+    }
+
+}
