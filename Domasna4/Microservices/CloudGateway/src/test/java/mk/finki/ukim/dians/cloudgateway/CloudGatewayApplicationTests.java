7cdbf1c86bf13d38b7e384739ab97d6e6304f79b
NikolaBlagoevski <88589626+NikolaBlagoevski@users.noreply.github.com>
2022-01-24 20:03:13 +0100

    Add files via upload
    Додавање на АПИ - микросервис

---
+++ Domasna4/Microservices/CloudGateway/src/test/java/mk/finki/ukim/dians/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.dians.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
